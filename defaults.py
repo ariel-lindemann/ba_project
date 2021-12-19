0d8272c169af3cdf45ca6315161fb5c4df16155b
Ariel Lindemann <ga92xan@mytum.de>
2021-12-19 18:41:16 +0100

    expanded defaults

--- defaults.py
+++ defaults.py
@@ -1,3 +1,4 @@
+# TODO read from config file
 CAMERA_NUMBER = 0
 STD_WAIT = 5
 
@@ -8,9 +9,11 @@ DEFAULT_MARKER_SIZE = 4
 # Positioning
 TOLERANCE = 0
 
-DATA_DIR = 'data'
+PROJECT_ROOT_DIR = '/Users/ariel/ba_project' #TODO change
+DATA_DIR = f'{PROJECT_ROOT_DIR}/data'
 # Calobration
 PARAMS_DIR = f'{DATA_DIR}/camera_parameters'
 TEMPLATE_IMG_PATH = f'{DATA_DIR}/alignment_templates/template.png'
 CALIBRATION_IMGS_PATH = f'{DATA_DIR}/calibration_images'
 CALIBRATION_RESULTS_PATH = f'{DATA_DIR}/calibration_results'
+CALIBRATION_IMGS_FORMAT = 'jpg'
