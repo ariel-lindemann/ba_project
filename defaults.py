6375aaa8b94c5b77b514d64292fa845e937b8918
Ariel Lindemann <ga92xan@mytum.de>
2021-12-29 08:49:05 +0100

    update defaults

--- defaults.py
+++ defaults.py
@@ -12,10 +12,15 @@ PROJECT_ROOT_DIR = '/Users/ariel/ba_project' #TODO change
 DATA_DIR = f'{PROJECT_ROOT_DIR}/data'
 # Calibration
 PARAMS_DIR = f'{DATA_DIR}/camera_parameters'
-TEMPLATE_IMG_PATH = f'{DATA_DIR}/alignment_templates/template.png'
+ALIGNMENT_TEMPLATE_IMG_PATH = f'{DATA_DIR}/alignment_templates/template.png'
 CALIBRATION_IMGS_PATH = f'{DATA_DIR}/calibration_images'
 CALIBRATION_RESULTS_PATH = f'{DATA_DIR}/calibration_results'
 CALIBRATION_IMGS_FORMAT = 'jpg'
 
 CALIBRATION_CORNERS_X = 6
 CALIBRATION_CORNERS_Y = 9
+
+# AGV
+DEFAULT_AGV_WIDTH = 380
+DEFAULT_AGV_LENGTH = 560
+DEFAULT_AGV_HEIGHT = 200
