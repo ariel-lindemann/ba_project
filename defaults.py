de8bc0ce46ab85321f34100d1b4f19d3446d9b26
Ariel Lindemann <ga92xan@mytum.de>
2021-12-31 02:12:52 +0100

    fix order

--- defaults.py
+++ defaults.py
@@ -12,11 +12,11 @@ PROJECT_ROOT_DIR = '/Users/ariel/ba_project' #TODO change
 DATA_DIR = f'{PROJECT_ROOT_DIR}/data'
 # Calibration
 PARAMS_DIR = f'{DATA_DIR}/camera_parameters'
+CALIBRATION_IMGS_FORMAT = 'jpg'
 ALIGNMENT_TEMPLATE_IMG_PATH = f'{DATA_DIR}/alignment_templates/template.png'
 CALIBRATION_PATTERN_PATH = f'{DATA_DIR}/calibration_pattern.{CALIBRATION_IMGS_FORMAT}'
 CALIBRATION_IMGS_PATH = f'{DATA_DIR}/calibration_images'
 CALIBRATION_RESULTS_PATH = f'{DATA_DIR}/calibration_results'
-CALIBRATION_IMGS_FORMAT = 'jpg'
 
 CALIBRATION_CORNERS_X = 6
 CALIBRATION_CORNERS_Y = 9
