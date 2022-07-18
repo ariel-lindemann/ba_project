1ed58bd571ed75eb949f618da70ba27d44408a2b
Ariel Lindemann <ga92xan@mytum.de>
2022-07-18 10:45:24 +0200

    renamed cluster to segment

--- segment.py
+++ segment.py
@@ -47,3 +47,49 @@ def cluster_dbscan(img, eps = 0.4, min_samples = 20):
     db = DBSCAN(eps= eps, min_samples=min_samples).fit(Z[:,:2])
     return np.uint8(db.labels_.reshape(img.shape[:2]))
     #TODO
+
+
+def code_masks(img, thr1=150, thr2=200):
+    '''
+    returns the masks of areas where codes could be
+    '''
+    #TODO flexible parameters
+    canny_img = cv2.Canny(img, thr1, thr2)
+    canny_blurred = cv2.GaussianBlur(canny_img, (101, 101), 0)
+    mask = cv2.threshold(canny_blurred, 50, 255, cv2.THRESH_BINARY)[1]
+
+    #TODO return masks
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    return cnts
+
+
+def draw_contours(img, cnts):
+
+    for (i, c) in enumerate(cnts):
+        (x, y, w, h) = cv2.boundingRect(c)
+        ((c_x, c_y), radius) = cv2.minEnclosingCircle(c)
+        cv2.circle(img, (int(c_x), int(c_y)), int(radius), (0, 0, 255), 3)
+        cv2.putText(img, "#{}".format(i + 1), (x, y - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+
+def image_segments(img, cnts):
+    imgs = []
+
+    for (i, c) in enumerate(cnts):
+        (x, y, w, h) = cv2.boundingRect(c)
+        segment = img[x:y, w:h]
+        imgs.append(segment)
+
+    return imgs
+
+
+def masked_img(img):
+    thr1=150 
+    thr2=200
+    canny_img = cv2.Canny(img, thr1, thr2)
+    canny_blurred = cv2.GaussianBlur(canny_img, (101, 101), 0)
+    thresh = cv2.threshold(canny_blurred, 50, 255, cv2.THRESH_BINARY)[1]
+
+    masked_img = cv2.bitwise_and(img, img, mask=thresh)
+    return masked_img
